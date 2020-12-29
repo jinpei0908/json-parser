2eb01d435e5c562582297c1fda10085c013dc7ec
jinpei0908 <jinpei0908@gmail.com>
2020-12-29 15:12:50 +0900

    interfaceを作った

---
+++ src/parser.rs
@@ -0,0 +1,13 @@
+pub fn parse_object() {}
+
+pub fn parse_array() {}
+
+pub fn parse_number() {}
+
+pub fn parse_string() {}
+
+pub fn parse_true() {}
+
+pub fn parse_false() {}
+
+pub fn parse_null() {}
